f709a1b5aab9151731b900c92ae750739f69ecdc
Yoann Collet <yoann.collet11@gmail.com>
2017-12-10 06:23:35 +0100

    Verif name and email

--- app/src/main/java/com/example/yoann/chat/NamePickerActivity.java
+++ app/src/main/java/com/example/yoann/chat/NamePickerActivity.java
@@ -3,17 +3,22 @@ package com.example.yoann.chat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class NamePickerActivity extends AppCompatActivity {
-
-    public static final String TAG = NamePickerActivity.class.getSimpleName();
+public class NamePickerActivity extends AppCompatActivity implements TextWatcher {
 
     EditText mNameEditText;
     EditText mEmailEditText;
     Button mSubmitButton;
+    TextView errorName;
+    TextView errorEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +28,98 @@ public class NamePickerActivity extends AppCompatActivity {
         mNameEditText = (EditText) findViewById(R.id.EditName);
         mEmailEditText = (EditText) findViewById(R.id.EditEmail);
 
+        errorName = (TextView) findViewById(R.id.error_name);
+        errorEmail = (TextView) findViewById(R.id.error_mail);
+
         mSubmitButton = (Button) findViewById(R.id.ButtonSubmit);
+
+        mNameEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (mNameEditText.getText().toString().isEmpty()) {
+                    showErrorName();
+                    return false;
+                } else {
+                    hideErrorName();
+                    return false;
+
+                }
+            }
+        });
+
+        mEmailEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (mEmailEditText.getText().toString().isEmpty()) {
+                    showErrorEmail();
+                    return false;
+                } else {
+                    hideErrorEmail();
+                    return false;
+
+                }
+            }
+        });
 
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String name = mNameEditText.getText().toString();
                 String mail = mEmailEditText.getText().toString();
-                UserStorage.saveUserInfo(getBaseContext(),name, mail);
-                Intent intent = new Intent(getBaseContext(), AppActivity.class);
-                startActivity(intent);
+                if (mNameEditText.getText().toString().isEmpty() && mEmailEditText.getText().toString().isEmpty()) {
+                    showErrorEmail();
+                    showErrorName();
+                } else if (mNameEditText.getText().toString().isEmpty()) {
+                    showErrorName();
+                    hideErrorEmail();
+                } else if (mEmailEditText.getText().toString().isEmpty()) {
+                    showErrorEmail();
+                    hideErrorName();
+                } else {
+                    UserStorage.saveUserInfo(getBaseContext(), name, mail);
+                    Intent intent = new Intent(getBaseContext(), AppActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
+    }
+
+    private void showErrorName() {
+        errorName.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorEmail() {
+        errorEmail.setVisibility(View.VISIBLE);
+    }
+
+    private void hideErrorName() {
+        errorName.setVisibility(View.INVISIBLE);
+    }
+
+    private void hideErrorEmail() {
+        errorEmail.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if (mNameEditText.getText().length() < 0) {
+            showErrorName();
+        } else if (mEmailEditText.getText().length() < 0) {
+            showErrorEmail();
+        } else {
+            hideErrorName();
+            hideErrorEmail();
+        }
     }
 }
